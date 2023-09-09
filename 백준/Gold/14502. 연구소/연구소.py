7faf00edac6da322599d5b7947de96bdd2851b27
PParkcode <80203360+PParkcode@users.noreply.github.com>
2023-09-09 17:41:14 +0900

    [Gold IV] Title: 연구소, Time: 2816 ms, Memory: 34296 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,83 +1,79 @@
-
 import sys
 import copy
+from collections import deque
 
-"""
-1. 데이터 입력 받기 V
-2. 모든 경우의 수에 대해 벽 3개 세우기 V
- 2.1 재귀를 사용?V
-3. 벽이 3개 세워지면 바이러스 퍼뜨림V
-4. 안전 영역 계산 V
-5. 데이터 원상복구 V
- --> arr 배열에는 벽만 설치
-    temp 배열에 arr 배열 값을 복사하고,
-    temp 배열에서 바이러스를 퍼뜨리고 안전영역 계산
-    arr 배열은 벽만 설치하고 그 데이터를 temp에 전해주기만 하면됨
-    
+sys.setrecursionlimit(10 ** 9)
 
-궁금한 점 
-3중 for문을 통해 모든 벽을 세울 수는 없을까?
-
-"""
-
-
-def wall(count):
-    global answer
-    if count == 3:
-        # 바이러스를 퍼뜨려야 함
-
-        # temp에 배열 복사
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = arr[i][j]
-
-        # 바이러스 전파
-        for i in range(n):
-            for j in range(m):
-                if arr[i][j] == 2:
-                    virus(j, i)
-        answer = max(countSafe(), answer)
-        return
-
-    # 모든 경우의 수에 대해 벽 세움
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] == 0:
-                arr[i][j] = 1
-                wall(count + 1)
-                arr[i][j] = 0
-
-
-def virus(x, y):
-    for k in range(4):
-        ny = y + dy[k]
-        nx = x + dx[k]
-        if nx < 0 or nx >= m or ny < 0 or ny >= n or temp[ny][nx] != 0:
-            continue
-        temp[ny][nx] = 2
-        virus(nx, ny)
-
-
-def countSafe():
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if temp[i][j] == 0:
-                count += 1
-
-    return count
-
-
-n, m = map(int, input().split())
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
-arr = []
-temp = [[0] * m for _ in range(n)]
 answer = 0
 
-for i in range(n):
-    arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
 
-wall(0)
+def solution():
+    n, m = map(int, input().split())
+    board = []
+    for i in range(n):
+        board.append(list(map(int, input().split())))
 
-print(answer)
+    wall(board, 0, n, m, 0, 0)
+
+    print(answer)
+
+
+# 벽을 세우는 함수
+def wall(board, count, n, m, a, b):
+    global answer
+
+    # 벽이 3개 세워지면 바이러스를 퍼뜨린다
+    if count == 3:
+        answer = max(answer, virus(board, n, m))
+        return
+
+    # 전체 배열을 순회하며 벽을 하나씩 세운다. 재귀를 이용해 모든 경우의 수에 대해 벽 3개를 세운다.
+    for i in range(a, n):
+        for j in range(m):
+            if i == a and j < b:
+                continue
+            if board[i][j] == 0:
+                board[i][j] = 1
+                wall(board, count + 1, n, m, i, j)  # count를 하나씩 증가해가며 벽을 세움
+                board[i][j] = 0
+
+
+# BFS를 이용하여 바이러스를 퍼뜨린다.
+def virus(board, n, m):
+    safeArea = 0
+    tempBoard = copy.deepcopy(board)
+    visited = [[True] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if tempBoard[i][j] == 2 and visited[i][j]:
+                bfs(tempBoard, visited, n, m, i, j)
+
+    # 바이러스를 모두 퍼뜨리면 안전 구역 계산후 반환
+    for i in range(n):
+        for j in range(m):
+            if tempBoard[i][j] == 0:
+                safeArea += 1
+    return safeArea
+
+
+# BFS를 이용한 바이러스 전파
+def bfs(tempBoard, visited, n, m, a, b):
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
+
+    q = deque()
+    q.append((a, b))
+    visited[a][b] = False
+    while q:
+        y, x = q.popleft()
+        tempBoard[y][x] = 2
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < m and 0 <= ny < n and tempBoard[ny][nx] == 0 and visited[ny][nx]:
+                q.append((ny, nx))
+                visited[ny][nx] = False
+
+
+solution()
