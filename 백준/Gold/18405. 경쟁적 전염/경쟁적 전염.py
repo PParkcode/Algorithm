38a302e6969c9dda4cab571a7ebb790362d3d7a9
PParkcode <80203360+PParkcode@users.noreply.github.com>
2023-09-12 18:21:38 +0900

    [Gold V] Title: 경쟁적 전염, Time: 136 ms, Memory: 34236 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,41 +1,73 @@
 import sys
+import copy
 from collections import deque
 
-
-def spread(v, ny, nx,count):
-    if ny < 0 or nx < 0 or ny > (n - 1) or nx > (n - 1) or arr[ny][nx] != 0 or count>=s:
-        return
-
-    arr[ny][nx] = v
-    q.append((v, ny, nx,count+1))
+sys.setrecursionlimit(10 ** 9)
 
 
-n, k = map(int, sys.stdin.readline().rstrip().split())
-arr = []
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+def solution():
+    n, k = map(int, input().split())
 
-temp = []
-q = deque()
-for _ in range(n):
-    arr.append(list(map(int, sys.stdin.readline().rstrip().split())))
-s, x, y = map(int, sys.stdin.readline().rstrip().split())
+    board = []
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] != 0:
-            # 모든 경우의 수를 값과 좌표를 큐에 담음
-            temp.append((arr[i][j], i, j, 0))
-temp.sort()
-q = deque(temp)
+    for i in range(n):
+        board.append(list(map(int, input().split())))
+    s, x, y = map(int, input().split())
+
+    virus(board, n, k, s)
+
+    print(board[x - 1][y - 1])
 
 
-# 요구하는 초만큼 반복
+def virus(board, n, k, s):
+    dx = [0, 0, -1, 1]
+    dy = [-1, 1, 0, 0]
 
-# 큐가 빌 때까지 값과 모든 경우의 수에 대해 바이러스 전파
-while q:
-    val, ty, tx, count = q.popleft()
-    for d in range(4):
-        spread(val, ty + dy[d], tx + dx[d],count)
+    # 바이러스 번호와 좌표를 담을 리스트
+    virusIndex = []
+    count = 0
+    time = 0
 
-print(arr[x - 1][y - 1])
+    q = deque()
+
+    # 바이러스를 담고 번호순 대로 정렬하여 큐에 담는다.
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] != 0:
+                virusIndex.append((i, j, board[i][j]))
+
+    sortedvirus = sorted(virusIndex, key=lambda x: (x[2]))
+
+    for item in sortedvirus:
+        q.append((item[0], item[1]))
+
+    # 큐 사이즈만큼 돌면 1초가 지났다고 생각
+    qsize = len(q)
+
+    # BFS로 진행
+    while q:
+
+        y, x = q.popleft()
+        count += 1
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n and board[ny][nx] == 0:
+                board[ny][nx] = board[y][x]
+                q.append((ny, nx))
+
+        # count를 하나씩 증가하면서 qSize와 같아지면 1초가 지난것이다.
+        # qsize를 현재 큐의 사이즈로 갱시해주고 count =0 시간은 1증가시킨다.
+        if count == qsize:
+            qsize = len(q)
+            count = 0
+            time += 1
+
+        # return 조건
+        if time == s:
+            return
+
+
+solution()
