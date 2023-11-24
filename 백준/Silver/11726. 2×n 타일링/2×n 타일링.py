ac39ae9e3a93761ce4e26df98868b690e8d527cb
PParkcode <80203360+PParkcode@users.noreply.github.com>
2023-11-24 19:14:35 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,17 @@
+def solution():
+    n = int(input())
+
+    dp = [0] * (n + 1)
+    if n ==1:
+        print(1)
+        return
+    
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007
+
+    print(dp[n])
+
+solution()
