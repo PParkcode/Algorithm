3e2ca5eb48f9ba5aabf3986acafbfc2e63419c33
PParkcode <80203360+PParkcode@users.noreply.github.com>
2024-01-08 23:25:27 +0900

    [Gold IV] Title: 플로이드, Time: 408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,31 +1,35 @@
-INF = 987654321
+import sys
+input = sys.stdin.readline
+
+INF = 2000000001
 
 
 def solution():
     n = int(input())
     m = int(input())
+
     graph = [[INF] * (n + 1) for _ in range(n + 1)]
-    
-    for _ in range(m):
+    for i in range(m):
         a, b, c = map(int, input().split())
         graph[a][b] = min(graph[a][b], c)
 
-    # 자기 자신 초기화
     for i in range(1, n + 1):
         graph[i][i] = 0
-    
-    # 플로이드 워셜 알고리즘
-    for k in range(1, n + 1):
-        for i in range(1, n + 1):
-            for j in range(1, n + 1):
-                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-    
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            for k in range(1, n + 1):
+                if j == k:
+                    continue
+                graph[j][k] = min(graph[j][k], graph[j][i] + graph[i][k])
+
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if graph[i][j] == INF:
                 graph[i][j] = 0
-            print(graph[i][j], end=' ')
+            print(graph[i][j], end=" ")
         print()
 
 
 solution()
+
