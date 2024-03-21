46049eeb9ee8f337760e44be584051cd16d49b01
PParkcode <80203360+PParkcode@users.noreply.github.com>
2024-03-21 22:15:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 37192 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,49 +1,48 @@
-import sys
 import heapq
-from collections import deque
 
-input = sys.stdin.readline
-
-INF = 987654321
-
+LIMIT = 250000
 
 def solution():
     n, k = map(int, input().split())
-    graph = [[] for _ in range(200000)]
-    distance = [INF] * 400001
-    for i in range(100000):
-        graph[i].append((i + 1, 1))
-        graph[i].append((i - 1, 1))
-        graph[i].append((2 * i, 0))
-    if n > k:
-        print(n - k)
+    
+    # 동생이 더 뒤에 있다면 할 필요 없음
+    if k<n:
+        print(n-k)
         return
+    else:
+        answer = search(n, k)
+        print(answer)
 
-    dijkstra(graph, distance, n, k)
 
-    print(distance[k])
-
-
-# 다익스트라를 사용해 최단 거리를 찾는다.
-def dijkstra(graph, distance, n, k):
+def search(n, k):
     q = []
-
+    visited = [True] * LIMIT
     heapq.heappush(q, (0, n))
-    distance[n] = 0
+    visited[n] = False
+
+    # 가질 수 있는 최대값
+    dif = k - n
 
     while q:
+        depth, place = heapq.heappop(q)
 
-        weight, num = heapq.heappop(q)
+        # 발견 했다면 depth 리턴
+        if place == k:
+            return depth
 
-        if weight > distance[num]:
-            continue
+        # 최대값을 넘어서는 2배로 이동할 필요 없음
+        if 2 * place - k <= dif and visited[2 * place]:
+            heapq.heappush(q, (depth, 2 * place))
+            visited[place * 2] = False
 
-        for item in graph[num]:
-            cost = weight + item[1]
+        # 최초 위치의 절반 이하로는 갈 필요 없음
+        if n//2 <= place - 1  and visited[place - 1]:
+            heapq.heappush(q, (depth + 1, place - 1))
+            visited[place - 1] = False
 
-            if cost < distance[item[0]]:
-                distance[item[0]] = cost
-                heapq.heappush(q, (cost, item[0]))
-
+        # 목표 지점을 넘겨서는 갈 필요 없음
+        if place + 1 <= k and visited[place + 1]:
+            heapq.heappush(q, (depth + 1, place + 1))
+            visited[place + 1] = False
 
 solution()
