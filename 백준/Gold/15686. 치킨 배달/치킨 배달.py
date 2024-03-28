c29be3ed7b25488f5a0a8aa7495d4ed831565a07
PParkcode <80203360+PParkcode@users.noreply.github.com>
2024-03-28 17:45:53 +0900

    [Gold V] Title: 치킨 배달, Time: 392 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,66 +1,65 @@
-from itertools import combinations
+INF = 987654321
+answer = INF
 
 
 def solution():
-    (n, m) = map(int, input().split())
-    arr = []
-    chickenList = []
-    houseList = []
-    for _ in range(n):
-        arr.append(list(map(int, input().split())))
+    n, m = map(int, input().split())
+    graph = []
+    # 치킨집과 가정집의 좌표를 담을 리스트
+    chickens = []
+    houses = []
 
     for i in range(n):
+        graph.append(list(map(int, input().split())))
+
+    # 치킨집과 집 좌표 저장
+    for i in range(n):
         for j in range(n):
-            if arr[i][j] == 2:
-                chickenList.append((i, j))
-            if arr[i][j] == 1:
-                houseList.append((i, j))
+            if graph[i][j] == 2:
+                chickens.append((i, j))
+            if graph[i][j] == 1:
+                houses.append((i, j))
+    # 재귀 시작
+    recur(chickens, set(), houses, 0, 0, m)
 
-    # 파이썬의 조합 기능 이용
-    # 재귀와 bfs이용시 시간 초과 발생 하여 이 기능 사용
-    candidates = list(combinations(chickenList, m))
-
-    print(selectbyCombination(arr, houseList, candidates, m))
+    print(answer)
 
 
-"""
-기존에는 bfs를 이용하여 거리를 구했지만 시간 초과가 났다.
-그래서 index를 이용해서 필요한 거리만 구하는 방식으로 수정
-"""
-def selectbyCombination(arr, houseList, candidates, m):
-    distanceSum = 0
-    chickenDistance = 999999999999
-    chicenIndex = []
+# 재귀문
+# 탈출 조건: count == answer 이면 치킨 거리 계산 후 answer 갱신
+# 선택한 치킨집이 m개가 될 때 까지 temp_chickens에 담는다.
+# m개가 된다면 치킨 거리 계산
+# 계산을 마친 후 계속 반복문 진행
+# temp_chickens는 시간 복잡도 최적화를 위해 set으로 설정 O(1)
+def recur(chickens, temp_chickens, houses, idx, count, m):
+    global answer
+    if count == m:
+        answer = min(answer, calculate(temp_chickens, houses))
+        return
 
-    # 후보군들을 하나씩 pop
-    while candidates:
-        distanceSum = 0
-        chicenIndex.clear()
-        index = candidates.pop(0)
-
-
-        for i in range(m):
-            chicenIndex.append((index[i][0], index[i][1]))
-
-        # 집 하나당 치킨 집과의 거리를 모두 비교함
-        for house in (houseList):
-            houseY = house[0]
-            houseX = house[1]
-            distance = 9999999999
-            for chicken in chicenIndex:
-                y = chicken[0] - houseY
-                x = chicken[1] - houseX
-                if y < 0:
-                    y = y * (-1)
-                if x < 0:
-                    x = x * (-1)
-                distance = min(distance, x + y)
-
-            distanceSum += distance
-
-        chickenDistance = min(distanceSum, chickenDistance)
+    for i in range(idx, len(chickens)):
+        y, x = chickens[i]
+        temp_chickens.add((y, x))
+        recur(chickens, temp_chickens, houses, i + 1, count + 1, m)
+        temp_chickens.remove((y, x))
 
 
-    return chickenDistance
+def calculate(temp_chickens, houses):
+    chicken_distance = 0
+
+    # 각각의 집들에 대해 살아남은 치킨집들 중 최소거리를 구하고,
+    # 최소 거리들을 합해 치킨 거리를 반환해 준다.
+    for house in houses:
+        distance = INF
+        house_y = house[0]
+        house_x = house[1]
+        for chicken in temp_chickens:
+            chicken_y = chicken[0]
+            chicken_x = chicken[1]
+            distance = min(distance, abs(house_y - chicken_y) + abs(house_x - chicken_x))
+        chicken_distance += distance
+
+    return chicken_distance
+
 
 solution()
